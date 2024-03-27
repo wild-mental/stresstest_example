7f83c2922655f502c18f1568524ea43666aa1849
wild-mental <wild.mental@kidsnote.com>
2024-03-28 02:34:04 +0900

    miscellaneous

--- src/main/java/kr/su/spring_rest/controller/TestController.java
+++ src/main/java/kr/su/spring_rest/controller/TestController.java
@@ -1,4 +1,4 @@
-package kr.swu.spring_rest.controller;
+package kr.su.spring_rest.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
